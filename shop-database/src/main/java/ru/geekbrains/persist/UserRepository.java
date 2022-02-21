c27177831ef4a995d7619809059ae8740d00dae5
VasichkinMikhail <budarain@mail.ru>
2022-02-21 17:58:46 +0300

    Revert "Интернет магазин с использованием SpringBoot"
    This reverts commit 487037a637d39fd9244edc447dc85f28c2771365.

--- shop-database/src/main/java/ru/geekbrains/persist/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.geekbrains.persist;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.geekbrains.persist.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
